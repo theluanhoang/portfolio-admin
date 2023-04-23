293a7957848226ba1ed41c3ace601c60bbe6514e
theluanhoang <hoangtheluan2016@gmail.com>
2023-04-23 10:40:40 +0700

    fix: fix current user

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,6 +1,6 @@
 import { FaUserAlt } from 'react-icons/fa'
 
-import { Link, useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import { HiPencilAlt } from 'react-icons/hi'
 import { FiLogOut } from 'react-icons/fi'
 import { auth } from '../../firebase';
@@ -42,14 +42,14 @@ function Sidebar() {
         <>
             <div className='fixed flex items-center z-50 top-[30px] right-[30px]'>
                 <li className={`relative sidebar-item group hover:bg-my-yellow`}>
-                    <Link to='/create-blog' className={`sidebar-item `}>
+                    <span className={`sidebar-item `}>
                         {
-                            user?.email !== '' ? <HiPencilAlt size={20} color='#fff' /> :
+                            user ? <HiPencilAlt size={20} color='#fff' /> :
                                 <FaUserAlt size={20} color='#fff' />
                         }
-                    </Link>
+                    </span>
                     {
-                        user?.email !== '' && (<div className='absolute group-hover:opacity-100 opacity-0 px-2 right-0 -bottom-[65px] rounded-md w-[100px] h-[50px] bg-[#2B2A2A]'>
+                        user && (<div className='absolute group-hover:opacity-100 opacity-0 px-2 right-0 -bottom-[65px] rounded-md w-[100px] h-[50px] bg-[#2B2A2A]'>
                             <div className='flex flex-row items-center gap-2 justify-center h-full' onClick={logout}>
                                 <p className='text-white font-semibold'>Logout</p> <FiLogOut size={20} color='#fff' />
                             </div>
